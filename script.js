0c20d8e2328b2228fb04c4258d4e452de1e00350
mohammed gouse <gousemohammed163@gmail.com>
2025-10-25 14:32:31 +0530

    feat: dynamically update background based on weather condition

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const card = document.querySelector(".card");
 const weatherIcon = document.querySelector(".weather-icon");
 const error = document.querySelector(".error");
 
@@ -28,15 +29,51 @@ async function checkWeather(ecity) {
     wind.innerHTML = data.wind.speed + " km/h";
 
     if (data.weather[0].main == "Clouds") {
+      //weatherIcon image
       weatherIcon.src = "images/clouds.png";
+      //weather condition image
+
+      card.style.background = "url(https://images.unsplash.com/photo-1506624682682-13fa0130f321?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687)";
+
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+
     } else if (data.weather[0].main == "Clear") {
+
       weatherIcon.src = "images/clear.png";
+
+      card.style.background = "url(https://images.unsplash.com/photo-1607280719353-3272826b21cc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687)";
+
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+
     } else if (data.weather[0].main == "Rain") {
+
       weatherIcon.src = "images/rain.png";
+
+      card.style.background = "url(https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735)";
+
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+
     } else if (data.weather[0].main == "Drizzle") {
+
       weatherIcon.src = "images/drizzle.png";
+
+      card.style.background = "url(https://images.unsplash.com/photo-1602755139240-dbbf8bb0c92f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687)";
+
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+
     } else if (data.weather[0].main == "Mist") {
+
       weatherIcon.src = "images/mist.png";
+
+      card.style.background = "url(https://images.unsplash.com/photo-1628534262701-0ada95e46152?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=627)";
+
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+
     }
 
     document.querySelector(".weather").style.display = "block";
